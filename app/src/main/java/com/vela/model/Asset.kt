8620768addf65f95c652cc1f7b236c2267767384
alirezasobht <163091659+alirezasobht@users.noreply.github.com>
2026-05-25 18:39:11 +1000

    add Asset model and fake data source

---
+++ app/src/main/java/com/vela/model/Asset.kt
@@ -0,0 +1,12 @@
+package com.vela.model
+
+data class Asset(
+    val id: String,
+    val symbol: String,
+    val name: String,
+    val image: String,
+    val currentPrice: Double,
+    val priceChangePercent24h: Double,
+    val marketCapRank: Int,
+    val sparkline: List<Double>
+)
