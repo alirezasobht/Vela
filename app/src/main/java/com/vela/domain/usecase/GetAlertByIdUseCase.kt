01d02972e5526054c98a51706f72c669af059de1
ali <alireza.sobh@gmail.com>
2026-06-18 16:52:49 +1000

    feature-alerts: alert use cases

---
+++ app/src/main/java/com/vela/domain/usecase/GetAlertByIdUseCase.kt
@@ -0,0 +1,13 @@
+package com.vela.domain.usecase
+
+import com.vela.domain.model.Alert
+import com.vela.domain.repository.AlertRepository
+import javax.inject.Inject
+
+class GetAlertByIdUseCase
+    @Inject
+    constructor(
+        private val repository: AlertRepository
+    ) {
+        suspend operator fun invoke(id: Long): Alert? = repository.getAlertById(id)
+    }
