9d95eb55a84a0edb2e60334229bf84a575475ca8
ali <alireza.sobh@gmail.com>
2026-07-28 19:13:33 +1000

    refactor-priceCache: delete dead codes

--- app/src/main/java/com/vela/domain/usecase/GetPricesUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.vela.domain.usecase
-
-import com.vela.domain.model.DataResult
-import com.vela.domain.model.SimplePrice
-
-fun interface GetPricesUseCase {
-    suspend operator fun invoke(ids: List<String>): DataResult<Map<String, SimplePrice>>
-}
--- app/src/main/java/com/vela/ui/base/pricepolling/PricePollingDelegate.kt
+++
@@ -1,9 +0,0 @@
-package com.vela.ui.base.pricepolling
-
-import com.vela.domain.model.AppError
-
-interface PricePollingDelegate {
-    fun getIds(): List<String>
-
-    fun onPriceError(error: AppError?)
-}
